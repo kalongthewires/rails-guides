ef2d03143cdeb83b036e4b70a52eac4e2f1433b0
Kim Hamper <kim.hamper@gmail.com>
2024-08-23 16:29:58 -0400

    Add comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   # ActiveRecord automatically declares attributes for every column
   # in the table so they don't have to be specified here
 
+  has_many :comments
+  
   # validations are checked before the model is saved
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
